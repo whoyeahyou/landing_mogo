2ae86bbd3ce7618ad64f105364a1a340462b540d
whoyeahyou <64033933+whoyeahyou@users.noreply.github.com>
2022-02-06 13:26:13 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -26,11 +26,17 @@ $(function () {
 
      $('[data-scroll').on('click', function (event) {
           event.preventDefault();
-          let blockId = $(this).data('scroll'),
+
+          let $this = $(this),
+               blockId = $this.data('scroll'),
                blockOffset = $(blockId).offset().top;
+
+          $('#nav a').removeClass('active');
+          $this.addClass('active');
+
           $('html, body').animate({
                scrollTop: blockOffset
-          }, 1500);
+          }, 1200);
      });
 
 
